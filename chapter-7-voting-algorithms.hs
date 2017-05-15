d86327362aad28373eaa316efdc8c2ca6140d98f
vosmann <vosmann@gmail.com>
2017-05-15 19:12:27 +0200

    Add voting algorithms.

---
+++ chapter-7-voting-algorithms.hs
@@ -0,0 +1,53 @@
+-- First past the post
+
+import Data.List
+
+votes :: [String]
+votes = ["Red", "Blue", "Green", "Blue", "Blue", "Red"]
+
+count :: Eq a => a -> [a] -> Int
+count x = length . filter (==x)
+
+rmdups :: Eq a => [a] -> [a]
+rmdups []     = []
+rmdups (x:xs) = x : filter (/=x) (rmdups xs)
+
+result :: Ord a => [a] -> [(Int, a)]
+result vs = sort [(count v vs, v) | v <- rmdups vs]
+
+winner :: Ord a => [a] -> a
+winner = snd . last . result
+
+-- Alternative vote
+
+ballots :: [[String]]
+ballots = [["Red", "Green"],
+           ["Blue"],
+           ["Green", "Red", "Blue"],
+           ["Blue", "Red", "Green"],
+           ["Green"]]
+
+rmempty :: Eq a => [[a]] -> [[a]]
+rmempty = filter (==[])
+
+elim :: Eq a => a -> [[a]] -> [[a]]
+elim b = map (filter (/=b)) 
+
+rank :: Ord a => [[a]] -> [a]
+rank = map snd . result . map head
+
+winner' :: Ord a => [[a]] -> a
+winner' bs = case rank (rmempty bs) of
+                [c]    -> c
+                (c:cs) -> winner' (elim c bs)
+
+
+
+
+
+
+
+
+
+
+
