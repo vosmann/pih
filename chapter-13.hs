796b7475c7c12ddf43ca00f31e2157d0cd5e00ac
vosmann <vosmann@gmail.com>
2017-05-28 13:09:04 +0200

    Chapter 13: expressions and calculator.

--- chapter-13.hs
+++ chapter-13.hs
@@ -81,11 +81,81 @@ char :: Char -> Parser Char
 char c = sat (== c)
 
 string :: String -> Parser String
-string []     = []
-string (x:xs) = 
+string []     = return []
+string (x:xs) = do char x
+                   string xs
+                   return (x:xs)
+
+ident :: Parser String
+ident = do x <- lower
+           xs <- many alphanum
+           return (x:xs)
+
+nat :: Parser Int
+nat = do xs <- some digit
+         return (read xs)
+
+space :: Parser ()
+space = do many (sat isSpace)
+           return ()
+
+int :: Parser Int
+int = do char '-'
+         n <- nat
+         return (-n)
+      <|> nat
+
+token :: Parser a -> Parser a
+token p = do space
+             t <- p
+             space
+             return t
+
+identifier :: Parser String
+identifier = token ident
+
+natural :: Parser Int
+natural = token nat
+
+integer :: Parser Int
+integer = token int
+
+symbol :: String -> Parser String
+symbol s = token (string s)
+
+nats :: Parser [Int]
+nats = do symbol "["
+          n <- natural
+          ns <- many (do symbol ","
+                         natural)
+          symbol "]"
+          return (n:ns)
 
 -- Arithmetic expressions
 
+factor :: Parser Int
+factor = do symbol "("
+            e <- expr
+            symbol ")"
+            return e
+          <|> integer
+
+term :: Parser Int
+term = do f <- factor
+          do symbol "*"
+             t <- term
+             return (f * t)
+           <|> return f
+
+expr :: Parser Int
+expr = do t <- term
+          do symbol "+"
+             e <- expr
+             return (t + e)
+           <|> do symbol "-"
+                  e <- expr
+                  return (t - e)
+           <|> return t
 
 -- Calculator
 
@@ -101,13 +171,80 @@ writeat :: Pos -> String -> IO ()
 writeat p xs = do goto p
                   putStr xs
 
+beep :: IO ()
+beep = putStr "\BEL"
+
 getCh :: IO Char
 getCh = do hSetEcho stdin False
            x <- getChar
            hSetEcho stdin True
            return x
+box :: [String]
+box = ["+---------------+",
+       "|               |",
+       "+---+---+---+---+",
+       "| q | c | d | = |",
+       "+---+---+---+---+",
+       "| 1 | 2 | 3 | + |",
+       "+---+---+---+---+",
+       "| 4 | 5 | 6 | - |",
+       "+---+---+---+---+",
+       "| 7 | 8 | 9 | * |",
+       "+---+---+---+---+",
+       "| 0 | ( | ) | / |",
+       "+---+---+---+---+"]
 
+buttons :: String
+buttons = standard ++ extra
+          where
+             standard = "qcd=123+456-789*0()/"
+             extra    = "QCD \ESC\BS\DEL\n"
 
+showbox :: IO ()
+showbox = sequence_ [writeat (1,y) b | (y,b) <- zip [1..] box]
+
+display xs = do writeat (3,2) (replicate 13 ' ')
+                writeat (3,2) (reverse (take 13 (reverse xs)))
+
+calc :: String -> IO ()
+calc xs = do display xs 
+             c <- getCh
+             if elem c buttons then
+                 process c xs
+             else
+                 do beep
+                    calc xs
+
+process :: Char -> String -> IO ()
+process c xs | elem c "qQ\ESC"    = quit
+             | elem c "dD\BS\DEL" = delete xs
+             | elem c "=\n"       = eval xs
+             | elem c "cC"        = clear
+             | otherwise          = press c xs
+
+quit :: IO ()
+quit = goto (1,14)
+
+delete :: String -> IO ()
+delete [] = calc []
+delete xs = calc (init xs)
+
+eval :: String -> IO ()
+eval xs = case parse expr xs of
+             [(n,[])] -> calc (show n)
+             _        -> do beep
+                            calc xs
+
+clear :: IO ()
+clear = calc []
+
+press :: Char -> String -> IO ()
+press c xs = calc (xs ++ [c])
+
+run :: IO ()
+run = do cls
+         showbox
+         clear
 
 -- Exercises
 
